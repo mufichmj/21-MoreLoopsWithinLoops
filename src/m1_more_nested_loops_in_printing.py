f8945e0ec177d414955d69720c422f183f76f7ae
mufichmj <mufichmj@rose-hulman.edu>
2018-05-07 00:36:10 -0400

    m1

--- src/m1_more_nested_loops_in_printing.py
+++ src/m1_more_nested_loops_in_printing.py
@@ -170,7 +170,7 @@ def vee(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -178,6 +178,15 @@ def vee(r):
     #   in this or the other problems in this module, as doing so would
     #   defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
+    for k in range(r):
+        for j in range(k):
+            print(' ', end='')
+        for i in range(r - k):
+            print(i + 1, end='')
+        print('-', end='')
+        for h in range(r - k):
+            print(h + 1, end='')
+        print()
 
 
 def run_test_numbers_constant_forward():
